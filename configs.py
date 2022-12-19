5c6413d073e545feef56b4fb8bed297be600f9a1
Janaki Raman <48284893+JOHN748@users.noreply.github.com>
2022-12-20 04:30:49 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -12,15 +12,15 @@ logging.basicConfig(
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", "29913092"))
+    API_HASH = os.environ.get("API_HASH", "21ad78b36fcdb7f48c1d01858d7fbfab")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5926536733:AAFhHrOn3KBtPNiHRsVxw4sU8KZopIOuwm8")
     DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads")
     LOGGER = logging
-    OWNER_ID = int(os.environ.get("OWNER_ID", 5339194287))
+    OWNER_ID = int(os.environ.get("OWNER_ID", 800954971))
     PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "0").split()))
     PRO_USERS.append(OWNER_ID)
-    MONGODB_URI = os.environ.get("MONGODB_URI", "")
-    FORCE_SUB = os.environ.get("FORCE_SUB", "beta_bot_updates")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100"))
+    MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://moviestrooper:hn5zyiPPhfgrROG2@cluster0.u7nh3fx.mongodb.net/?retryWrites=true&w=majority")
+    FORCE_SUB = os.environ.get("FORCE_SUB", "mtmovies002")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001810941379"))
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
