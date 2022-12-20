f4c7b2dfe1995a0abfbb5c30ef39b5f5a1f4d946
Kaneki Bots <93288020+newkanekibot@users.noreply.github.com>
2022-12-20 11:09:27 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Jeolpaul/Ultra-Renamer-and-Watermark.git /Ultra-Renamer-and-Watermark    
+  git clone https://github.com/newkanekibot/URB.git /URB    
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Ultra-Renamer-and-Watermark
