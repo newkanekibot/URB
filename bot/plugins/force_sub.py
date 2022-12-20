cf9b141ae35d033ec7d826d7aa2362142deb69d8
Janaki Raman <48284893+JOHN748@users.noreply.github.com>
2022-12-20 06:11:50 +0530

    Update force_sub.py

--- bot/plugins/force_sub.py
+++ bot/plugins/force_sub.py
@@ -7,8 +7,8 @@ from pyrogram.errors import UserNotParticipant
 @Client.on_message(filters.private & filters.create(not_subscribed))
 async def is_not_subscribed(client, message):
     await message.reply_text(
-       text="**⚠️Sorry bro,You didn't Joined Our Updates Channel Join now and start again🙏**",
+       text="**𝚂𝙾𝚁𝚁𝚈 𝙳𝚄𝙳𝙴, 𝚈𝙾𝚄 𝙲𝙰𝙽'𝚃 𝚄𝚂𝙴 𝙼𝙴!\n𝙸'𝙼 𝙰 𝙿𝚁𝙸𝚅𝙰𝚃𝙴 𝙱𝙾𝚃 🤖\n𝙲𝙾𝙽𝚃𝙰𝙲𝚃 𝚃𝙷𝙸𝚂 𝙶𝚄𝚈, 𝚃𝙾 𝚄𝚂𝙴 𝙼𝙴!**",
        reply_markup=InlineKeyboardMarkup([
-           [ InlineKeyboardButton(text="📢𝙹𝚘𝚒𝚗 𝙼𝚢 𝚄𝚙𝚍𝚊𝚝𝚎 𝙲𝚑𝚊𝚗𝚗𝚎𝚕📢", url=client.invitelink)]
+           [ InlineKeyboardButton(text="😬😈𝚂𝚃𝙰𝚁 𝙻𝙾𝚁𝙳😈😬", url='https://t.me/JOHN748')]
            ])
        )
